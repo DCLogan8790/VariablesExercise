e88323463b4cf5d6c803a0431a9371447a0d20f1
Chelsea Logan <chelsealogan@SCWDBs-MacBook-Pro.local>
2021-02-23 11:48:31 -0600

    1st complete

---
+++ variablesExercise.js
@@ -0,0 +1,46 @@
+// 1
+// const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+
+// 4
+num += 7;
+
+// 5
+num--;
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7
+let blank;
+
+// 8
+// undefinded
+console.log(blank);
+
+
+// 9.
+blank = null;
+
+
+// Bonus 
+
+// 1B
+let quater = .25, dime = 0.10, nickel = 0.05, penny = 0.01;  
+
+// 2B
+console.log(quater,dime,nickel,penny);
+
+// 3B
+let letters = "qwertyuiop"
+
+// 4B
+
+
+console.log(letters[9]);
